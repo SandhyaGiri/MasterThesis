Fixing bug in dpn_loss file - dir mean  was calc as softmax of exp(logits)

robust_priornet/attack_priornet.py
@@ -115,6 +115,8 @@ def perform_epsilon_attack(model: nn.Module, adv_dataset: Dataset, correct_class
         ood_uncertainties = UncertaintyEvaluator(ood_logits).get_all_uncertainties()
         ood_eval_dir = os.path.join(result_dir, 'ood_eval')
         os.makedirs(ood_eval_dir)
+        for key in uncertainties.keys():
+            np.savetxt(os.path.join(ood_eval_dir, key._value_ + '.txt'), uncertainties[key])
         OutOfDomainDetectionEvaluator(uncertainties, ood_uncertainties, ood_eval_dir).eval()
 
     # return aversarial successes
@@ -156,7 +158,7 @@ def main():
                               None,
                               'train' if args.train_dataset else 'test')
 
-    dataset = DataSpliter.reduceSize(dataset, 5000)
+    # dataset = DataSpliter.reduceSize(dataset, 5000)
     org_dataset_folder = os.path.join(args.result_dir, "org-images")
     if not os.path.exists(org_dataset_folder):
         os.makedirs(org_dataset_folder)
@@ -180,7 +182,7 @@ def main():
                                       trans.get_transforms(),
                                       None,
                                       'train' if args.train_dataset else 'test')
-        ood_dataset = DataSpliter.reduceSize(ood_dataset, 5000)
+        # ood_dataset = DataSpliter.reduceSize(ood_dataset, 5000)
 
     # perform attacks on the same dataset, using different epsilon values.
     adv_success_rates = []

robust_priornet/eval/out_of_domain_detection.py
@@ -7,12 +7,12 @@ from .uncertainty import UncertaintyMeasuresEnum
 
 
 class OutOfDomainDetectionEvaluator:
-    
+
     def __init__(self, id_uncertainty_measures, ood_uncertainty_measures, result_dir):
         self.id_uncertainty_measures = id_uncertainty_measures
         self.ood_uncertainty_measures = ood_uncertainty_measures
         self.result_dir = result_dir
-        
+
     def get_target_labels(self):
         """
         Generates target labels for the ood detection task as a binary classification
@@ -23,7 +23,7 @@ class OutOfDomainDetectionEvaluator:
         id_labels = np.zeros_like(self.id_uncertainty_measures[UncertaintyMeasuresEnum.CONFIDENCE])
         ood_labels = np.ones_like(self.ood_uncertainty_measures[UncertaintyMeasuresEnum.CONFIDENCE])
         return np.concatenate((id_labels, ood_labels), axis=0) # row wise concatenation
-    
+
     def eval(self):
         """
         Uses the combined uncertainty measure (of id and ood) as the model's predictive

robust_priornet/eval/uncertainty.py
@@ -101,10 +101,9 @@ class UncertaintyEvaluator(BaseUncertaintyEvaluator):
         return -1 * np.sum(dirichlet_mean * digamma_term, axis=1)
 
     def get_differential_entropy(self):
-        log_term = np.sum(gammaln(self.alphas) - gammaln(self.alpha_0), axis=1)
-        digamma_term = np.sum((self.alphas - 1) *
-                              (digamma(self.alphas) - digamma(self.alpha_0)), axis=1)
-        return log_term - digamma_term
+        log_term = gammaln(self.alphas) - gammaln(self.alpha_0)
+        digamma_term = (self.alphas - 1) * (digamma(self.alphas) - digamma(self.alpha_0))
+        return np.sum(log_term - digamma_term, axis=1)
 
 class UncertaintyEvaluatorTorch(BaseUncertaintyEvaluator):
     """
@@ -136,7 +135,6 @@ class UncertaintyEvaluatorTorch(BaseUncertaintyEvaluator):
         return -1 * torch.sum(dirichlet_mean * digamma_term, dim=1) # sum across classes
 
     def get_differential_entropy(self):
-        log_term = torch.sum(torch.lgamma(self.alphas) - torch.lgamma(self.alpha_0), dim=1)
-        digamma_term = torch.sum((self.alphas - 1) *
-                                 (torch.digamma(self.alphas) - torch.digamma(self.alpha_0)), dim=1)
-        return log_term - digamma_term
+        log_term = torch.lgamma(self.alphas) - torch.lgamma(self.alpha_0)
+        digamma_term = (self.alphas - 1) *(torch.digamma(self.alphas) - torch.digamma(self.alpha_0))
+        return torch.sum(log_term - digamma_term, dim=1)

robust_priornet/losses/dpn_loss.py
@@ -80,7 +80,7 @@ class KLDivDirchletDistLoss:
         """
         Computes KL divergence KL( Dir(alpha = target_precision * target_mean) || Dir(beta = precision * mean)
         """
-        precision_term = torch.lgamma(target_precision) - torch.lgamma(precision)
+        precision_term = torch.lgamma(target_precision + epsilon) - torch.lgamma(precision + epsilon)
 
         alphas = target_precision * target_mean
         betas = precision * mean
@@ -94,7 +94,7 @@ class KLDivDirchletDistLoss:
         # print("Given dim alphas: ", alphas.shape, " labels: ", labels.shape if labels is not None else '')
         k = alphas.shape[1] # num_classes
         precision = torch.sum(alphas, dim=1, keepdim=True)
-        mean = F.softmax(alphas, dim=1)
+        mean = alphas / precision
         
         if labels is None:
             # ood sample, set all alphas to 1 to get a flat simplex or precision = num_classes, mean = 1/precision

robust_priornet/training/trainer.py
@@ -206,11 +206,11 @@ class PriorNetTrainer:
         # returns average metrics (loss, accuracy, dirichlet_dist_precision)
         return {
             'loss': np.round(kl_loss, 4),
-            'id_loss': np.round(id_loss, 2),
-            'ood_loss': np.round(ood_loss, 2),
+            'id_loss': np.round(id_loss, 4),
+            'ood_loss': np.round(ood_loss, 4),
             'id_accuracy': np.round(100.0 * accuracies, 2),
-            'id_precision': np.round(id_precision, 2),
-            'ood_precision': np.round(ood_precision, 2)
+            'id_precision': np.round(id_precision, 4),
+            'ood_precision': np.round(ood_precision, 4)
         }
 
 
@@ -269,9 +269,9 @@ class PriorNetTrainer:
 
         return {
             'loss': np.round(kl_loss, 4),
-            'id_loss': np.round(id_loss, 2),
-            'ood_loss': np.round(ood_loss, 2),
+            'id_loss': np.round(id_loss, 4),
+            'ood_loss': np.round(ood_loss, 4),
             'id_accuracy': np.round(100.0 * accuracies, 2),
-            'id_precision': np.round(id_precision, 2),
-            'ood_precision': np.round(ood_precision, 2)
+            'id_precision': np.round(id_precision, 4),
+            'ood_precision': np.round(ood_precision, 4)
         }

robust_priornet/utils/persistence.py
@@ -24,7 +24,7 @@ def persist_image_dataset(dataset, mean, std, n_channels, image_dir):
     assert isinstance(dataset, Dataset), ("Dataset is not of right type. Cannot be persisted.")
     images = []
     testloader = DataLoader(dataset, batch_size=128,
-                            shuffle=False, num_workers=4)
+                            shuffle=False, num_workers=4, pin_memory=True)
     with torch.no_grad():
         for _, data in enumerate(testloader, 0):
             image, _ = data

robust_priornet/utils/visualizer.py
@@ -53,21 +53,25 @@ def get_epoch_summaries(model_dir, num_epochs, verbose=False):
     val_ood_loss = []
     train_id_accuracy = []
     val_id_accuracy = []
+    train_loss = []
+    val_loss = []
     for epoch in range(num_epochs):
         summary = torch.load(os.path.join(model_dir, f'epoch_summary_{epoch+1}.pt'))
         if verbose:
             print(summary)
         train_id_loss.append(summary['train_results']['id_loss'])
         train_ood_loss.append(summary['train_results']['ood_loss'])
+        train_loss.append(summary['train_results']['loss'])
         train_id_accuracy.append(summary['train_results']['id_accuracy'])
         val_id_loss.append(summary['val_results']['id_loss'])
         val_ood_loss.append(summary['val_results']['ood_loss'])
+        val_loss.append(summary['val_results']['loss'])
         val_id_accuracy.append(summary['val_results']['id_accuracy'])
 
-    return train_id_loss, train_ood_loss, val_id_loss, val_ood_loss, train_id_accuracy, val_id_accuracy
+    return train_id_loss, train_ood_loss, val_id_loss, val_ood_loss, train_id_accuracy, val_id_accuracy, train_loss, val_loss
 
 def plot_loss_accuracy_curve(model_dir, num_epochs):
-    train_id_loss, train_ood_loss, val_id_loss, val_ood_loss, train_id_accuracy, val_id_accuracy = get_epoch_summaries(model_dir, num_epochs)
+    train_id_loss, train_ood_loss, val_id_loss, val_ood_loss, train_id_accuracy, val_id_accuracy, train_loss, val_loss = get_epoch_summaries(model_dir, num_epochs)
     figure, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 8))
 
     # ID axis for loss curve
@@ -95,6 +99,14 @@ def plot_loss_accuracy_curve(model_dir, num_epochs):
                y_label='Accuracy', title='Accuracy curve',
                curve_title='ID - Validation accuracy', show_legend=True)
 
+    # Overall loss axis
+    plot_curve(x_values, train_loss, axes[1,1], x_label='Epochs',
+               y_label='Loss', title='Loss curve',
+               curve_title='Train loss', show_legend=True)
+    plot_curve(x_values, val_loss, axes[1,1], x_label='Epochs',
+               y_label='Loss', title='Loss curve',
+               curve_title='Validation loss', show_legend=True)
+    
     figure.tight_layout()
     plt.show()
     
@@ -135,10 +147,10 @@ def plot_adv_samples(org_eval_dir, attack_dir, epsilon, plots_dir='vis', known_m
         adv_success: number of previously correctly classified samples that got misclassified under attack.
     """
     target_epsilon_dir = os.path.join(attack_dir, f"e{epsilon}-attack")
-    probs = np.loadtxt(f"{target_epsilon_dir}/probs.txt")
-    labels = np.loadtxt(f"{target_epsilon_dir}/labels.txt")
+    probs = np.loadtxt(f"{target_epsilon_dir}/eval/probs.txt")
+    labels = np.loadtxt(f"{target_epsilon_dir}/eval/labels.txt")
     # current confidence on attack images
-    new_confidence = np.loadtxt(f"{target_epsilon_dir}/confidence.txt")
+    new_confidence = np.loadtxt(f"{target_epsilon_dir}/eval/confidence.txt")
 
     # misclassified samples under attack
     preds = np.argmax(probs, axis=1)
@@ -155,6 +167,7 @@ def plot_adv_samples(org_eval_dir, attack_dir, epsilon, plots_dir='vis', known_m
     old_preds = np.argmax(old_probs, axis=1)
     correct_classifications = np.asarray(old_preds == labels, dtype=np.int32)
     correct_classified_indices = np.argwhere(correct_classifications == 1)
+    print("# Correct classified samples prior attack: ", len(correct_classified_indices))
 
     misclassified = np.intersect1d(misclassified, correct_classified_indices)
     print("# Real adversarial samples under attack: ", misclassified.size)