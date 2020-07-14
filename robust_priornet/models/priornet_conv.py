cad2c67d6c19f9b6c7521e283be6d400afcff93a
Sandhya Giri <sandhya.giri@tum.de>
2020-07-14 20:13:10 +0200

    Added result table for new thresholds

--- robust_priornet/models/priornet_conv.py
+++ robust_priornet/models/priornet_conv.py
@@ -53,7 +53,7 @@ class CustomVGG(nn.Module):
     def forward(self, x):
         x = self.features(x)
         x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
+        x = x.reshape((x.size(0), -1))
         x = self.classifier(x)
         return x
 
